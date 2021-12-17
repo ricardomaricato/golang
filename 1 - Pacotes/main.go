be627465d85bc213c8ca667f65a3339caa1a76f4
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-12-17 12:46:21 -0300

    feat: Pacotes Externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	err := checkmail.ValidateFormat("ricardo.maricato@gmail.com")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
