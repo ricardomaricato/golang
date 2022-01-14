b29702bb36efe8c55df73afe81f8b8924400052a
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-14 18:30:22 -0300

    crud: buscar usuario por id

--- 24 - Crud Básico/servidor/servidor.go
+++ 24 - Crud Básico/servidor/servidor.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type usuario struct {
@@ -91,6 +94,43 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
 		w.Write([]byte("Erro ao converter os usuários para JSON"))
+		return
+	}
+}
+
+// BuscarUsuario traz um usuário específico salvo no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+		return
+	}
+
+	linha, erro := db.Query("select * from usuarios where id = ?", ID)
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar o usuário!"))
+		return
+	}
+
+	var usuario usuario
+	if linha.Next() {
+		if erro := linha.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário!"))
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para JSON!"))
 		return
 	}
 }
